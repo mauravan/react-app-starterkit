416f11ad30bd842364273d1c334c7b2a5e7ae3b2
roe <rom-meier@hotmail.com>
2022-02-02 09:37:09 +0100

    add basic form with styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { initAuth } from '@formio/react';
 import { useAuth, AuthPage } from './modules/auth';
 import { FormProvider, FormsPage } from './modules/forms/form';
 import { Footer, Header, Home, Loading, Modal } from './common';
-import { AppConfig } from './config';
+import { Form } from 'react-formio';
 import './App.scss';
 import EventsPage from './modules/events/components/EventsPage';
 import { Alerts, AlertsProvider } from './modules/alerts';
@@ -34,13 +34,7 @@ function App() {
         <AlertsProvider>
           <Alerts/>
           <div className="container" id="main">
-            { AppConfig.projectUrl === 'https://reactstarter.form.io' ?
-                <div className="alert alert-warning">
-                  This app is still configured to use the default project.
-                  Be sure to create your own project in form.io and change the PROJECT_URL in src/config.js
-                </div>
-                : null
-              }
+            <Form src="https://btywzajnwlxpiym.form.io/erstbewilligung" />
             <Route exact path="/" component={Home} />
             <Route path="/form" component={FormsPage} />
             <Route path="/event" render={(props) => <FormProvider><EventsPage {...props} /></FormProvider>} />
